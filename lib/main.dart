2a168edb50d1034cb082b6a33a396b6c40d8fed5
Horus125 <ignacykuchcinski@gmail.com>
2019-03-14 03:09:09 +0100

    Redesign UI and improve text field

--- lib/main.dart
+++ lib/main.dart
@@ -13,18 +13,46 @@ class Calculator extends StatefulWidget {
 }
 
 class CalculatorState extends State<Calculator> {
-  String editText = '';
+  TextSelection currentSelection =
+      TextSelection(baseOffset: 0, extentOffset: 0);
+  TextEditingController controller = TextEditingController(text: '1234');
   void append(String character) {
     setState(() {
-      editText += character;
+      if (controller.selection.baseOffset >= 0) {
+        currentSelection = TextSelection(
+            baseOffset: controller.selection.baseOffset + 1,
+            extentOffset: controller.selection.extentOffset + 1);
+        controller.text =
+            controller.text.substring(0, controller.selection.baseOffset) +
+                character +
+                controller.text.substring(
+                    controller.selection.baseOffset, controller.text.length);
+        controller.selection = currentSelection;
+      } else {
+        controller.text += character;
+      }
     });
   }
 
   void clear([bool longPress = false]) {
     setState(() {
-      editText = longPress
-          ? editText = ''
-          : editText.substring(0, editText.length - 1);
+      if (longPress) {
+        controller.text = '';
+      } else {
+        if (controller.selection.baseOffset >= 0) {
+          currentSelection = TextSelection(
+              baseOffset: controller.selection.baseOffset - 1,
+              extentOffset: controller.selection.extentOffset - 1);
+          controller.text = controller.text
+                  .substring(0, controller.selection.baseOffset - 1) +
+              controller.text.substring(
+                  controller.selection.baseOffset, controller.text.length);
+          controller.selection = currentSelection;
+        } else {
+          controller.text =
+              controller.text.substring(0, controller.text.length - 1);
+        }
+      }
     });
   }
 
@@ -32,13 +60,16 @@ class CalculatorState extends State<Calculator> {
     setState(() {
       Parser p = new Parser();
       try {
-        editText = editText.replaceAll('e+', 'e');
-        editText = editText.replaceAll('e', '*10^');
-        Expression exp = p.parse(editText);
+        controller.text = controller.text.replaceAll('e+', 'e');
+        controller.text = controller.text.replaceAll('e', '*10^');
+        Expression exp = p.parse(controller.text);
         num outcome = exp.evaluate(EvaluationType.REAL, ContextModel());
-        editText = outcome.toStringAsPrecision(13).replaceAll(RegExp(r'0+$'), '').replaceAll(RegExp(r'\.$'), '');
+        controller.text = outcome
+            .toStringAsPrecision(13)
+            .replaceAll(RegExp(r'0+$'), '')
+            .replaceAll(RegExp(r'\.$'), '');
       } catch (e) {
-        editText = 'Error';
+        controller.text = 'Error';
       }
     });
   }
@@ -46,18 +77,20 @@ class CalculatorState extends State<Calculator> {
   Widget _buildButton(Color color, String label, Function() func) {
     return Expanded(
       flex: 1,
-      child: Container(
-        padding: EdgeInsets.all(1.0),
-        child: GestureDetector(
-          onLongPress: (label == 'C') ? () => clear(true) : null,
-          child: FlatButton(
-            child: Text(
-              label,
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40.0),
+      child: GestureDetector(
+        onLongPress: (label == 'C') ? () => clear(true) : null,
+        child: FlatButton(
+          shape: BeveledRectangleBorder(),
+          child: Text(
+            label,
+            style: TextStyle(
+                fontSize: 32.0,
+                color: Colors.white,
+                fontWeight: FontWeight.w300
             ),
-            color: color,
-            onPressed: func,
           ),
+          color: color,
+          onPressed: func,
         ),
       ),
     );
@@ -65,37 +98,42 @@ class CalculatorState extends State<Calculator> {
 
   @override
   Widget build(BuildContext context) {
+    Color numColor = Color.fromRGBO(48, 47, 63, .94);
+    Color opColor = Color.fromRGBO(22, 21, 29, .93);
     return MaterialApp(
       title: 'Calculator',
       theme: ThemeData(
-        primarySwatch: Colors.grey,
+        primarySwatch: Colors.green,
+        accentColor: Colors.greenAccent,
       ),
       home: Scaffold(
-        appBar: AppBar(title: Text('Dahlia Calculator')),
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).canvasColor,
+          elevation: 0.0,
+        ),
         body: Column(
           crossAxisAlignment: CrossAxisAlignment.end,
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             Expanded(
-                child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                  Text(
-                    editText,
-                    style: TextStyle(fontSize: 30.0),
-                    maxLines: 1,
-                  ),
-                ])),
+              flex: 3,
+              child: TextField(
+                controller: controller,
+                decoration: null,
+                textAlign: TextAlign.right,
+                style: TextStyle(fontWeight: FontWeight.w300, fontSize: 80.0),
+                focusNode: AlwaysDisabledFocusNode(),
+              ),
+            ),
             Expanded(
               child: Row(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  _buildButton(Colors.orange, 'C', clear),
-                  _buildButton(Colors.orange, '(', () => append('(')),
-                  _buildButton(Colors.orange, ')', () => append(')')),
-                  _buildButton(Colors.orange, '\u00F7', () => append('/')),
+                  _buildButton(opColor, 'C', clear),
+                  _buildButton(opColor, '(', () => append('(')),
+                  _buildButton(opColor, ')', () => append(')')),
+                  _buildButton(opColor, '\u00F7', () => append('/')),
                 ],
               ),
             ),
@@ -104,10 +142,10 @@ class CalculatorState extends State<Calculator> {
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  _buildButton(Colors.orange[400], '7', () => append('7')),
-                  _buildButton(Colors.orange[400], '8', () => append('8')),
-                  _buildButton(Colors.orange[400], '9', () => append('9')),
-                  _buildButton(Colors.orange, '\u00D7', () => append('*')),
+                  _buildButton(numColor, '7', () => append('7')),
+                  _buildButton(numColor, '8', () => append('8')),
+                  _buildButton(numColor, '9', () => append('9')),
+                  _buildButton(opColor, '\u00D7', () => append('*')),
                 ],
               ),
             ),
@@ -116,10 +154,10 @@ class CalculatorState extends State<Calculator> {
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  _buildButton(Colors.orange[400], '4', () => append('4')),
-                  _buildButton(Colors.orange[400], '5', () => append('5')),
-                  _buildButton(Colors.orange[400], '6', () => append('6')),
-                  _buildButton(Colors.orange, '-', () => append('-')),
+                  _buildButton(numColor, '4', () => append('4')),
+                  _buildButton(numColor, '5', () => append('5')),
+                  _buildButton(numColor, '6', () => append('6')),
+                  _buildButton(opColor, '-', () => append('-')),
                 ],
               ),
             ),
@@ -128,10 +166,10 @@ class CalculatorState extends State<Calculator> {
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  _buildButton(Colors.orange[400], '1', () => append('1')),
-                  _buildButton(Colors.orange[400], '2', () => append('2')),
-                  _buildButton(Colors.orange[400], '3', () => append('3')),
-                  _buildButton(Colors.orange, '+', () => append('+')),
+                  _buildButton(numColor, '1', () => append('1')),
+                  _buildButton(numColor, '2', () => append('2')),
+                  _buildButton(numColor, '3', () => append('3')),
+                  _buildButton(opColor, '+', () => append('+')),
                 ],
               ),
             ),
@@ -140,10 +178,10 @@ class CalculatorState extends State<Calculator> {
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  _buildButton(Colors.orange[400], '00', () => append('00')),
-                  _buildButton(Colors.orange[400], '0', () => append('0')),
-                  _buildButton(Colors.orange[400], '.', () => append('.')),
-                  _buildButton(Colors.orange, '=', equals),
+                  _buildButton(numColor, '%', () => append('/100')),
+                  _buildButton(numColor, '0', () => append('0')),
+                  _buildButton(numColor, '.', () => append('.')),
+                  _buildButton(opColor, '=', equals),
                 ],
               ),
             ),
@@ -152,4 +190,9 @@ class CalculatorState extends State<Calculator> {
       ),
     );
   }
+}
+
+class AlwaysDisabledFocusNode extends FocusNode {
+  @override
+  bool get hasFocus => false;
 }
